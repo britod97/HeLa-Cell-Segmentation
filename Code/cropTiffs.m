9b8ba19d7bf0ae13d8bf442216bc24f40ceb2424
britod97 <britod97@gmail.com>
2026-08-05 16:52:05 +0100

    Update cropTiffs.m

--- Code/cropTiffs.m
+++ Code/cropTiffs.m
@@ -23,7 +23,7 @@ function cropTiffs(tiffdir, x, y, z_lo, z_hi, w, h, outdir)
         I = imread(infile);
 
         % Crop
-        Ic = imcrop(I, [x y w-1 h-1]);
+        Ic = histeq(imcrop(I, [x y w-1 h-1]));
 
         % Save
         outfile = fullfile(outdir, files(i).name);
