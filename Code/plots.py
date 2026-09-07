faa6ad5fc4b6ba70ab6e6a14ac43d45e3c97cdc8
britod97 <britod97@gmail.com>
2026-09-07 16:17:30 +0100

    Added plots by cell.

--- Code/plots.py
+++ Code/plots.py
@@ -134,6 +134,64 @@ for d_type in distance_types:
 
 
 ###############################################################################
-# Morphology comparisons
+# Orientation of mitochondria with respect to the CENTROID by CELL
+
+n_bins = 9
+normalised_heights = True
+bar_width = 1
+
+angle_types = ['centroid_angle', 'surface_angle']
+
+figure_dir = Path(f'D:/GitHub Repos/HeLa_Cell_Data/CIL50051/Figures/ROSE_isotropic/{angle}/by_cell')
+if not os.path.exists(figure_dir):
+    os.mkdir(figure_dir)
+
+for angle in angle_types:
+    for roi_name in ROIs:
+        fig = plt.figure(figsize=(10,10))
+        mito_data = all_mito_data.loc[all_mito_data['ROI'] == roi_name]
+        hist = np.histogram(mito_data[angle], bins=n_bins, range=(0, np.pi))
+    
+        radii = hist[0]
+        if normalised_heights:
+            radii = radii/np.sum(radii)
+    
+        theta = [(hist[1][k]+hist[1][k+1])/2 for k in range(len(hist[1])-1)]
+        width = bar_width*(np.pi/n_bins)
+    
+        # Coloring
+        norm = colors.Normalize(vmin=0, vmax=0.6)
+        cmap = plt.cm.jet
+        C = cmap(norm(radii))
+    
+        # Plotting
+        ax = plt.subplot(projection='polar')
+        chart = ax.bar(theta, radii, width=width, bottom=0.0,
+               color=C, alpha=1, edgecolor='k', lw=3, zorder=10)
+    
+        ax.set_title(f'{roi_name} {angle}')
+        ax.set_yticks([0, 0.1, 0.2, 0.3, 0.4])
+        ax.set_ylim(0, 0.4)
+        ax.set_thetalim(0, np.pi)
+        ax.set_xticks([0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi])
+        ax.tick_params(axis='y', labelsize=36, pad=10)
+        ax.tick_params(axis='x', labelsize=42, pad=30)
+        ax.grid('both', lw=2, zorder=-10, alpha=0.8)
+        for spine in ax.spines.values():
+            spine.set_linewidth(3)
+    
+        # --- Separate colorbar export ---
+        fig_cb, ax_cb = plt.subplots(figsize=(1, 6))  # (width, height)
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        sm.set_array([])
+    
+        cbar = plt.colorbar(sm, cax=ax_cb)
+        cbar.set_label('Normalised Frequency', rotation=270, labelpad=15)
+    
+        # Save only the colorbar
+        fig_cb.savefig(f'{figure_dir}/colorbar.pdf', dpi=300, bbox_inches='tight', transparent=True)
+        plt.close(fig_cb)
+    
+        plt.savefig(f'{figure_dir}/{roi_name[:11]}_{angle}.pdf')
 
 
